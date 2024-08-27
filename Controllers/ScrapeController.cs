eec4b406b592de7a016d62d23a152e5ef53e8c8a
Garrett Kerr <gkerr@cloudcover.it>
2024-08-27 15:46:59 -0700

    - Small changes for a cleaner controller - Added default values

--- Controllers/ScrapeController.cs
+++ Controllers/ScrapeController.cs
@@ -34,43 +34,42 @@ namespace WebsiteScraper.Controllers
         {
             this.log.LogInformation("Website Scrape POST function processed a request.");
 
+            var scraper = scraperFactory(website.ToLower());
+            if (scraper == null)
+            {
+                this.log.LogError(message: $"Website Scrape POST function Invalid website value: {website}");
+                return new NotFoundObjectResult(new { Error = $"Invalid website value: {website}" }) { StatusCode = 404 };
+            }
+
+            // Grab the user input from the POST body
+            var search = request.Query;
+            var location = request.Location;
+            var dayRange = request.LastNDays;
+            if (!ValidateDayRange(dayRange))
+            {
+                return new BadRequestObjectResult(new { Error = $"Invalid lastndays value: {dayRange}" }) { StatusCode = 403 };
+            }
+
             try
             {
-                var scraper = scraperFactory(website.ToLower());
-                if (scraper == null)
-                {
-                    this.log.LogError(message: $"Website Scrape POST function Invalid website value: {website}");
-                    return new NotFoundObjectResult(new { Error = $"Invalid website value: {website}" }) { StatusCode = 404 };
-                }
-
-                var search = request.Query;
-                var location = request.Location;
-                var dayRange = request.LastNDays;
-                if (dayRange != null)
-                {
-                    if (dayRange < 0)
-                    {
-                        this.log.LogError(message: $"Website Scrape POST function Invalid lastNDays value: {dayRange}");
-                        return new BadRequestObjectResult(new { Error = $"Invalid lastndays value: {dayRange}" }) { StatusCode = 403 }; ;  // Returns a 404 Not Found response
-                    }
-                }
-
-                try
-                {
-                    var jobDataList = await scraper.ScrapeJobs(search, location, dayRange);
-                    return Ok(jobDataList);
-                }
-                catch (Exception ex)
-                {
-                    this.log.LogError(message: "Unexpected error on Website Scrape POST .", exception: ex);
-                    return new ObjectResult(ex.Message) { StatusCode = 500 };
-                }
+                var jobDataList = await scraper.ScrapeJobs(search, location, dayRange);
+                return Ok(jobDataList);
             }
             catch (Exception ex)
             {
                 this.log.LogError(message: "Unexpected error on Website Scrape POST .", exception: ex);
                 return new ObjectResult(ex.Message) { StatusCode = 500 };
-            }            
+            }          
+        }
+
+        private bool ValidateDayRange(int? dayRange)
+        {
+            if (dayRange.HasValue && dayRange < 0)
+            {
+                this.log.LogError($"Invalid lastNDays value: {dayRange}");
+                return false;
+            }
+            return true;
         }
     }
 }
